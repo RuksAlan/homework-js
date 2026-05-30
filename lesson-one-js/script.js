14faf8faa25e666d5ad7ee9f4763c6b93e141dad
RuksAlan <153196584+RuksAlan@users.noreply.github.com>
2026-05-30 20:13:35 +0300

    Add files via upload

---
+++ lesson-one-js/script.js
@@ -0,0 +1,23 @@
+// Задача 1
+
+const firstName = "Алёна";
+const lastName = "Анурина";
+const isStudent = true;
+
+// Задача 2
+
+let age = 27;
+let currentYear = 2026;
+let birthYear = currentYear - age;
+
+console.log(`Мой год рождения: ${birthYear}`);
+
+// Задача 3
+
+console.log(`Меня зовут ${firstName} ${lastName}, мне ${age} лет.
+Я ученица курса: ${isStudent}`);
+
+// Задача 4
+
+// 123456789falsetrue
+console.log("Ответ: 123456789falsetrue");
